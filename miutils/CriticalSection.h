50791d8e2cfa6540488c4c532129f5bb16ba8066
Marcel Siegwart <marcel.siegwart@gmx.ch>
2023-10-15 10:15:59 +0200

    add makefile,CriticalSection

---
+++ miutils/CriticalSection.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <mutex>
+#include <pthread.h>
+
+class CriticalSection
+{
+private:
+	pthread_mutex_t _CriticalSection;
+public:
+	CriticalSection()
+	{
+		pthread_mutex_init(&_CriticalSection, NULL);
+	}
+	~CriticalSection()
+	{
+		pthread_mutex_destroy(&_CriticalSection);
+	}
+	void EnterCriticalSection()
+	{
+		pthread_mutex_lock(&_CriticalSection);
+	}
+	void LeaveCriticalSection()
+	{
+		pthread_mutex_unlock(&_CriticalSection);
+	}
+};
+
